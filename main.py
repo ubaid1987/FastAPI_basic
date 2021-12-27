154bca7feca63591c8511e53df44c80bba34a12d
Muhammad Ubaid <ubaidullah_khan@hotmail.com>
2021-12-27 16:09:21 +0500

    FastAPI First Commit

--- main.py
+++ main.py
@@ -6,17 +6,7 @@ from fastapi import FastAPI
 
 app = FastAPI(title="Ubaid App",
     description="My First Attempt",
-    version="0.0.1",
- #   terms_of_service="http://example.com/terms/",
- #   contact={
- #       "name": "Deadpoolio the Amazing",
- #       "url": "http://x-force.example.com/contact/",
-#       "email": "dp@x-force.example.com",
- #   },
-# #   license_info={
- #       "name": "Apache 2.0",
-  #      "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
- #   },
+    version="2.4.1",
     )
 
 @app.get("/")
