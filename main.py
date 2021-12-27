8467c4e1afff137bc187d15467c79c05f71c1620
Muhammad Ubaid <ubaidullah_khan@hotmail.com>
2021-12-27 16:05:20 +0500

    FastAPI First Commit

--- main.py
+++ main.py
@@ -3,7 +3,21 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from fastapi import FastAPI
-app = FastAPI()
+
+app = FastAPI(title="Ubaid App",
+    description="My First Attempt",
+    version="0.0.1",
+ #   terms_of_service="http://example.com/terms/",
+ #   contact={
+ #       "name": "Deadpoolio the Amazing",
+ #       "url": "http://x-force.example.com/contact/",
+#       "email": "dp@x-force.example.com",
+ #   },
+# #   license_info={
+ #       "name": "Apache 2.0",
+  #      "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+ #   },
+    )
 
 @app.get("/")
 def index():
@@ -13,6 +27,10 @@ def index():
 def about():
     return {"Data":{"About": "FastAPI"}}
 
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"Data ":{"Description ": "Unpublished Data"}}
+
 @app.get("/blog/{id}")
-def blog(id):
+def blog(id: int):
     return {"Data":{"Blog ID": id}}
